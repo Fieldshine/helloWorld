e7a1fc41e31b01d22eeef4a5dee129146135d9f8
田野 <field@Tomek-MacBook-Pro.local>
2015-10-08 14:34:16 +0800

    mygit

---
+++ mygit/mygit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mygit
+//
+//  Created by 田野 on 15/10/8.
+//  Copyright (c) 2015年 田野. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mygit/mygit/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  mygit
+//
+//  Created by 田野 on 15/10/8.
+//  Copyright (c) 2015年 田野. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"nidaye ");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
